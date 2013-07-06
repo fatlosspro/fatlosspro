9c75cea39191d4bdf23326a9ab1c44ba8a6aa316
Alexander Khovansky <heidar.enm@gmail.com>
2013-07-07 03:41:05 +0400

    TinyMCE production bug. Crap. #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ FatLossPro::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.precompile += %w[tinymce/tiny_mce.js tinymce/langs/en.js tinymce/themes/advanced/editor_template.js tinymce/plugins/youtube/editor_plugin.js tinymce/plugins/youtube/js/youtube.js]
   # Generate digests for assets URLs.
   config.assets.digest = true
 
