c278e6614eeea907f115680960f1508bb214fa45
Alexander Khovansky <heidar.enm@gmail.com>
2013-07-07 04:03:52 +0400

    Dashboard fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @current_weight = current_user.weights.last.value
+    @current_weight = current_user.weights.last.value || "Your last logged weight will appear here"
   end
 
 end
